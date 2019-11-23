743ca0f20e7f2cb1bde869c46942a3a8e8f445a6
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-11-23 00:27:35 -0500

    Made player thinner.

--- game.py
+++ game.py
@@ -7,8 +7,7 @@ pyg.init()
 
 class Player:
   def __init__(self, x, y, size, angle):
-    self.x = x
-    self.y = y
+    self.pos = [x, y]
     self.size = size
     self.radius = size/2
     self.angle = angle
@@ -42,9 +41,9 @@ while not game_over:
         if event.type == pyg.QUIT:
             sys.exit()
     ##### player coordinate and angle update #####
-    player_tip = (player.x + 4/3 * player.radius * math.cos(player.angle), player.y - 4/3 * player.radius * math.sin(player.angle))
-    player_rear_left = (player.x - player.radius * (2/3 * math.cos(player.angle) + math.sin(player.angle)), player.y + player.radius * (2/3 * math.sin(player.angle) - math.cos(player.angle)))
-    player_rear_right = (player.x - player.radius * (2/3 * math.cos(player.angle) - math.sin(player.angle)), player.y + player.radius * (2/3 * math.sin(player.angle) + math.cos(player.angle)))
+    player_tip = (player.pos[0] + 4/3 * player.radius * math.cos(player.angle), player.pos[1] - 4/3 * player.radius * math.sin(player.angle))
+    player_rear_left = (player.pos[0] - player.radius * (2/3 * math.cos(player.angle) + math.sin(player.angle)), player.pos[1] + player.radius * (2/3 * math.sin(player.angle) - math.cos(player.angle)))
+    player_rear_right = (player.pos[0] - player.radius * (2/3 * math.cos(player.angle) - math.sin(player.angle)), player.pos[1] + player.radius * (2/3 * math.sin(player.angle) + math.cos(player.angle)))
     keys_pressed = pyg.key.get_pressed()
     if keys_pressed[pyg.K_ESCAPE]:
         sys.exit()
@@ -65,35 +64,35 @@ while not game_over:
     if player.is_thrusting:
         player.thrust[0] += PLAYER_THRUST * math.cos(player.angle)
         player.thrust[1] -= PLAYER_THRUST * math.sin(player.angle)
-        fire_tip = (player.x - 5/3 * player.radius * math.cos(player.angle), player.y + 5/3 * player.radius * math.sin(player.angle))
-        fire_rear_left = (player.x - player.radius * (2/3 * math.cos(player.angle) + 0.5 * math.sin(player.angle)), player.y + player.radius * (2/3 * math.sin(player.angle) - 0.5 * math.cos(player.angle)))
-        fire_rear_right = (player.x - player.radius * (2/3 * math.cos(player.angle) - 0.5 * math.sin(player.angle)), player.y + player.radius * (2/3 * math.sin(player.angle) + 0.5 * math.cos(player.angle)))
+        fire_tip = (player.pos[0] - 5/3 * player.radius * math.cos(player.angle), player.pos[1] + 5/3 * player.radius * math.sin(player.angle))
+        fire_rear_left = (player.pos[0] - player.radius * (2/3 * math.cos(player.angle) + 0.5 * math.sin(player.angle)), player.pos[1] + player.radius * (2/3 * math.sin(player.angle) - 0.5 * math.cos(player.angle)))
+        fire_rear_right = (player.pos[0] - player.radius * (2/3 * math.cos(player.angle) - 0.5 * math.sin(player.angle)), player.pos[1] + player.radius * (2/3 * math.sin(player.angle) + 0.5 * math.cos(player.angle)))
         ##### draw fire #####
         pyg.draw.polygon(screen, FIRE_FILL_COLOR, [fire_tip, fire_rear_left, fire_rear_right])
-        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_tip, fire_rear_left, width=player.size//10)
-        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_rear_left, fire_rear_right, width=player.size//10)
-        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_tip, fire_rear_right, width=player.size//10)
+        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_tip, fire_rear_left, width=player.size//12)
+        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_rear_left, fire_rear_right, width=player.size//12)
+        pyg.draw.line(screen, FIRE_OUTLINE_COLOR, fire_tip, fire_rear_right, width=player.size//12)
     else:
         player.thrust[0] -= FRICTION * player.thrust[0]
         player.thrust[1] -= FRICTION * player.thrust[1]
     ##### draw player #####
-    pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_left, width=player.size//10)
-    pyg.draw.line(screen, PLAYER_COLOR, player_rear_left, player_rear_right, width=player.size//10)
-    pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_right, width=player.size//10)
+    pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_left, width=player.size//15)
+    pyg.draw.line(screen, PLAYER_COLOR, player_rear_left, player_rear_right, width=player.size//15)
+    pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_right, width=player.size//15)
     ##### centroid check test #####
-    pyg.draw.rect(screen, TEXT_COLOR, (player.x-1, player.y-1, 2, 2))
+    pyg.draw.rect(screen, TEXT_COLOR, (player.pos[0]-1, player.pos[1]-1, 2, 2))
     ###############################
     player.angle += player.rotation
-    player.x += player.thrust[0]
-    player.y += player.thrust[1]
+    player.pos[0] += player.thrust[0]
+    player.pos[1] += player.thrust[1]
     ##### back on screen when gone off screen #####
-    if player.x < -player.radius:
-        player.x = WIDTH + player.radius
-    elif player.x > WIDTH + player.radius:
-        player.x = -player.radius
-    if player.y < -player.radius:
-        player.y = HEIGHT + player.radius
-    elif player.y > HEIGHT + player.radius:
-        player.y = -player.radius
+    if player.pos[0] < -player.radius:
+        player.pos[0] = WIDTH + player.radius
+    elif player.pos[0] > WIDTH + player.radius:
+        player.pos[0] = -player.radius
+    if player.pos[1] < -player.radius:
+        player.pos[1] = HEIGHT + player.radius
+    elif player.pos[1] > HEIGHT + player.radius:
+        player.pos[1] = -player.radius
     screen.blit(text, textRect)
     pyg.display.flip()
