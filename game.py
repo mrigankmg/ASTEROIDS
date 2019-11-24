f5a68a0e0aa38e4f43dfe5460adcba4689f13a4d
Mrigank Mehta <mrigank.mehta@mail.utoronto.ca>
2019-11-24 00:04:14 -0500

    Created asteroids but they don't move.

--- game.py
+++ game.py
@@ -1,8 +1,27 @@
 import sys
 import pygame as pyg
 import math
+import random
 
 pyg.init()
+
+WIDTH = 1280
+HEIGHT = 720
+BACKGROUND_COLOR = (0, 0, 0) #Black
+TEXT_COLOR = (255, 255, 255) #White
+PLAYER_COLOR = (245, 66, 66) #Red
+FIRE_FILL_COLOR = (255, 202, 54) #Yellow
+FIRE_OUTLINE_COLOR = (255, 136, 0) #Orange
+ASTEROID_COLOR = (235, 235, 235) #Light Grey
+TEXT_POS = (25, 15)
+TURN_SPEED = math.radians(10)
+PLAYER_SIZE = 40
+PLAYER_THRUST = 0.1 #acceleration of ship in px/s
+FRICTION = 0.01
+NUM_ASTEROIDS = 3
+ASTEROID_SPEED = 1
+ASTEROID_SIZE = 100
+ASTEROID_VERTICES = 10
 
 class Player:
   def __init__(self, x, y, size, angle):
@@ -14,24 +33,33 @@ class Player:
     self.is_thrusting = False
     self.thrust = [0, 0]
 
-WIDTH = 1280
-HEIGHT = 720
-BACKGROUND_COLOR = (0, 0, 0) #Black
-TEXT_COLOR = (255, 255, 255) #White
-PLAYER_COLOR = (245, 66, 66) #Red
-FIRE_FILL_COLOR = (255, 202, 54) #Yellow
-FIRE_OUTLINE_COLOR = (255, 136, 0) #Orange
-TEXT_POS = (25, 15)
-player = Player(WIDTH/2, HEIGHT/2, 40, 90)
-TURN_SPEED = math.radians(10)
-PLAYER_THRUST = 0.1 #acceleration of ship in px/s
-FRICTION = 0.01
+class Asteroid:
+  def __init__(self, x, y, size):
+    self.pos = [x, y]
+    self.xv = random.random() * ASTEROID_SPEED * (1 if random.random() < 0.5 else -1)
+    self.yv = random.random() * ASTEROID_SPEED * (1 if random.random() < 0.5 else -1)
+    self.size = size
+    self.radius = size/2
+    self.angle =  math.radians(random.random() * 360)
+    self.vertices = math.floor(random.random() * (ASTEROID_VERTICES + 1) + ASTEROID_VERTICES/2)
 
+asteroids = []
+
+def createAsteroidBelt():
+    global asteroids
+    asteroids = []
+    for i in range(NUM_ASTEROIDS):
+        x = math.floor(random.random() * WIDTH)
+        y = math.floor(random.random() * HEIGHT)
+        asteroids.append(Asteroid(x, y, ASTEROID_SIZE))
+
+player = Player(WIDTH/2, HEIGHT/2, PLAYER_SIZE, 90)
 screen = pyg.display.set_mode((WIDTH, HEIGHT))
 pyg.display.set_caption('Asteroids Neural Network')
 font = pyg.font.Font('trench100free.ttf', 40)
 text = font.render('SCORE:', True, TEXT_COLOR, BACKGROUND_COLOR)
 game_over = False
+createAsteroidBelt()
 
 while not game_over:
     for event in pyg.event.get():
@@ -76,6 +104,17 @@ while not game_over:
     pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_left, width=player.size//15)
     pyg.draw.line(screen, PLAYER_COLOR, player_rear_left, player_rear_right, width=player.size//15)
     pyg.draw.line(screen, PLAYER_COLOR, player_tip, player_rear_right, width=player.size//15)
+
+    ##### draw asteroids #####
+    for ast in asteroids:
+        for i in range(ast.vertices):
+            start = (ast.pos[0] + ast.radius * math.cos(ast.angle + i * math.pi * 2 / ast.vertices), ast.pos[1] + ast.radius * math.sin(ast.angle + i * math.pi * 2 / ast.vertices))
+            if i == ast.vertices - 1:
+                end = (ast.pos[0] + ast.radius * math.cos(ast.angle), ast.pos[1] + ast.radius * math.sin(ast.angle))
+            else:
+                end = (ast.pos[0] + ast.radius * math.cos(ast.angle + (i + 1) * math.pi * 2 / ast.vertices), ast.pos[1] + ast.radius * math.sin(ast.angle + (i + 1) * math.pi * 2 / ast.vertices))
+            pyg.draw.line(screen, ASTEROID_COLOR, start, end, width=player.size//15)
+
     ##### centroid check test #####
     # pyg.draw.rect(screen, TEXT_COLOR, (player.pos[0]-1, player.pos[1]-1, 2, 2))
     ###############################
